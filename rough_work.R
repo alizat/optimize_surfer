f36097c269c261fb603ff766dfc8f1b627285a2a
Ali Ezzat <ali_ezzat85@yahoo.com>
2022-08-21 11:08:10 +0200

    scrape subtitle, synopsis & author

--- rough_work.R
+++ rough_work.R
@@ -136,42 +136,52 @@ suppressMessages({
 library(rvest)
 link <- "https://www.heroic.us/optimize"
 page <- read_html(link)
-categories <- 
+missions <- 
   page %>% 
   html_nodes(".formatted-type p strong a") %>% 
   html_attr('href') %>% 
   set_names(html_text(html_nodes(page, ".formatted-type p strong a")))
 
 titles <- tibble()
-for (i in 1:length(categories)) {
-  current_category <- categories[i]
-  print(glue('{current_category}'))
-  subpage <- read_html(current_category)
+for (i in 1:length(missions)) {
+  current_mission <- missions[i]
+  print(glue('{current_mission}'))
+  subpage <- read_html(current_mission)
   title <-
     subpage %>% 
     html_nodes("header a h2") %>% 
     html_text() %>% 
     str_squish()
+  subtitle <- 
+    subpage %>% 
+    html_nodes(".content header") %>% 
+    # html_nodes("header") %>% 
+    # keep(~ .x %>% str_detect('class="title"')) %>% 
+    # discard(~ .x %>% str_detect('role="banner"')) %>% 
+    map_chr(~ .x %>% html_nodes("a+ div") %>% html_text())
+  author <- 
+    subpage %>% 
+    html_nodes(".feed-item") %>% 
+    keep(~ .x %>% str_detect('h2')) %>% 
+    map_chr(~ .x %>% html_nodes("div span a") %>% html_text() %>% str_squish() %>% paste(collapse = ' & '))
   title_type <-
     subpage %>% 
     html_nodes(".badge") %>% 
     html_text() %>% 
     str_squish() %>% 
     discard(~ .x == 'Locked')
-  # subtitle <-
-  #   subpage %>% 
-  #   html_nodes("header div span") %>% 
-  #   html_text() %>% 
-  #   str_squish()
   synopsis <- 
     subpage %>% 
-    html_nodes(".clamp-sm") %>% 
-    html_text() %>% 
-    str_squish()
-  titles <- rbind(titles, tibble(title, title_type, synopsis) %>% mutate(category = names(current_category)))
+    html_nodes(".feed-item") %>% 
+    keep(~ .x %>% str_detect('h2')) %>% 
+    map_chr(~ .x %>% html_nodes(".clamp-sm") %>% html_text() %>% str_squish() %>% paste(collapse = ', '))
+  titles <- 
+    rbind(titles, 
+          tibble(title, 
+                 subtitle, 
+                 author, 
+                 synopsis, 
+                 title_type) %>% mutate(mission = names(current_mission)))
   Sys.sleep(5)
 }
-
-
-
 
